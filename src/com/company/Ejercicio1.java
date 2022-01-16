028e64249872d437d421b04112eec90a8c4b659f
Daniela <felesdaniela@gmail.com>
2022-01-16 10:10:45 +0100

    Initial commit

---
+++ src/com/company/Ejercicio1.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        byte variable1 = 5;
+        short variable2 = 10;
+        int variable3 = 30;
+        long variable4 = 100;
+
+        float variable5 = 5.5f;
+        double variable6 = 10.5d;
+
+        boolean variable7 = false;
+        boolean variable8 = true;
+
+        char variable9 = 'a';
+        String variable10 = "Hola";
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+        System.out.println(variable7);
+        System.out.println(variable8);
+        System.out.println(variable9);
+        System.out.println(variable10);
+
+    }
+}
